328da22698a51750ab424d3aec98c35ce5d55a03
马 小磊 <maxl@baiten.cn>
2018-02-08 16:34:13 +0800

    test

--- src/main/java/com/baitengsoft/utils/TestMain.java
+++ src/main/java/com/baitengsoft/utils/TestMain.java
@@ -1,12 +1,17 @@
 package com.baitengsoft.utils;
 
 import com.baitengsoft.framework.annotation.Controller;
+import com.baitengsoft.framework.annotation.Request;
 import com.baitengsoft.framework.annotation.Service;
 import com.baitengsoft.test.AnnotationClassTemplate;
+import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -71,6 +76,39 @@ public class TestMain {
                 return cls.isAnnotationPresent(annotationClass);
             }
         }.getClassList();
+
+        for (Class<?> actionClass : list){
+
+            Method[] actionMethods = actionClass.getDeclaredMethods();
+            for (Method actionMethod : actionMethods){
+
+                if (actionMethod.isAnnotationPresent(Request.Get.class)){
+                    String requestPath = actionMethod.getAnnotation(Request.Get.class).value();
+                    assert requestPath != null;
+                }
+            }
+        }
         assert list != null;
+    }
+
+    @Test
+    public void testSQL()
+    {
+        //dataBaseConfigItemRepository.getDataBaseConfigItem("ZHLX","2");
+       /* LinkedTreeMap<Long,String> testMap = new LinkedTreeMap();
+        TreeMap map = new TreeMap();
+        map.descendingMap();
+        testMap.put(4L,"sdadad");
+        testMap.put(2L,"sdffffadad");*/
+
+        Map<String,String> hashMap = new HashMap();
+        hashMap.put("1","sdadad");
+        hashMap.put("2","sdffffadad");
+
+        for(Map.Entry<String,String> maps : hashMap.entrySet()){
+
+            hashMap.remove(maps.getKey());
+        }
+        String str = "";
     }
 }
